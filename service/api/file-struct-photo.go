e43d7c7683cc116cbd16e8f68cf1bcab5804f239
Alessio_Borgi <73711270+alessioborgi@users.noreply.github.com>
2022-11-20 20:41:09 +0100

    Add Photo Checks

--- service/api/file-struct-photo.go
+++ service/api/file-struct-photo.go
@@ -1,11 +1,16 @@
 package api
 
+import (
+	"strings"
+)
+
+type Phrase string
 type Photo struct {
-	Photoid        string   `json:"photoid"`
+	Photoid        int      `json:"photoid"`
 	Filename       string   `json:"filename"` //Note here it will be in bynary.
 	UploadDate     string   `json:"uploadDate"`
 	Location       Location `json:"location"`
-	Phrase         string   `json:"phrase"`
+	Phrase         Phrase   `json:"phrase"`
 	NumberLikes    int      `json:"numberLikes"`
 	NumberComments int      `json:"numberComments"`
 }
@@ -15,11 +20,28 @@ type Location struct {
 	longitude float64
 }
 
+func (l Location) ValidLocation() bool {
+	return l.latitude <= 90 && l.latitude >= -90 && l.longitude <= 180 && l.longitude >= -180
+}
+
+func (p Phrase) ValidPhrase() bool {
+	return len(p) >= 5 && len(p) <= 1000
+}
+
+func (p Photo) ValidPhoto() bool {
+	return p.Location.ValidLocation() && p.Phrase.ValidPhrase() && p.Photoid >= 0 &&
+		p.NumberLikes >= 0 && p.NumberComments >= 0
+}
+
 type Comment struct {
 	Commentid         int    `json:"commentid"`
 	CommenterUsername string `json:"commenterUsername"`
-	Phrase            string `json:"phrase"`
+	Phrase            Phrase `json:"phrase"`
 	UploadDate        string `json:"uploadDate"`
+}
+
+func (c Comment) ValidComment() bool {
+	return c.Commentid >= 0 && c.Phrase.ValidPhrase()
 }
 
 type Like struct {
@@ -31,6 +53,11 @@ type Ban struct {
 	Banid      string `json:"banid"` //This corresponds to the Username of the User Banned.
 	UploadDate string `json:"uploadDate"`
 	Motivation string `json:"motivation"`
+}
+
+func (b Ban) ValidBan() bool {
+	b_lower := strings.ToLower(b.Motivation)
+	return b_lower == "spam" || b_lower == "bad behaviour" || b_lower == "threats"
 }
 
 type Follow struct {
