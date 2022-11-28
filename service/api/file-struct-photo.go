21420274a57b5c55b78547e05bf3930a4eae70d7
Alessio_Borgi <73711270+alessioborgi@users.noreply.github.com>
2022-11-28 07:17:41 +0100

    Add Checks for Photo, Ban & Follow

--- service/api/file-struct-photo.go
+++ service/api/file-struct-photo.go
@@ -1,18 +1,64 @@
 package api
 
 import (
+	"fmt"
+	"strconv"
 	"strings"
+	"time"
 )
 
 type Phrase string
+type DateTime string
+type Motivation string
+
+var current_hour, current_minute, current_second = time.Now().Clock()
+
 type Photo struct {
 	Photoid        int      `json:"photoid"`
 	Filename       byte     `json:"filename"` //Note here it will be in bynary.
-	UploadDate     string   `json:"uploadDate"`
+	UploadDate     DateTime `json:"uploadDate"`
 	Location       Location `json:"location"`
 	Phrase         Phrase   `json:"phrase"`
 	NumberLikes    int      `json:"numberLikes"`
 	NumberComments int      `json:"numberComments"`
+}
+
+// Declaring a Method for checking the UploadDate validity w.r.t. its validity.
+func (d DateTime) ValidUploadDate() bool { //yyyy/mm/dd
+	date_time := strings.Split(string(d), "T") //here find the way to also include "/"
+	date := strings.Split(string(date_time[0]), "-")
+	time := strings.Split(string(date_time[1]), ":")
+	year, erry := strconv.Atoi(date[0])
+	month, errm := strconv.Atoi(date[1])
+	day, errd := strconv.Atoi(date[2])
+	hour, errh := strconv.Atoi(time[0])
+	minute, errmi := strconv.Atoi(time[1])
+	second, errs := strconv.Atoi(time[2][:2]) //For not taking the "Z" character.
+
+	if erry != nil && errm != nil && errd != nil && errh != nil && errmi != nil && errs != nil {
+		// Check whether the date is after the 2022-01-01T00:00:00Z and before the current date (range included).
+		if len(d) == 20 && year <= 2022 && year <= current_year && month >= 1 && month <= 12 && month <= current_month && day >= 1 && current_day <= 31 && day <= current_day && hour >= 0 && hour <= 24 && hour <= current_hour && minute >= 0 && minute <= 60 && minute <= second && second >= 0 && second <= 60 && second <= current_second {
+			if month == 2 && (day != 29 && day != 30 && day != 31) {
+				if (month == 4 || month == 6 || month == 9 || month == 11) && (day != 31) {
+					fmt.Println("Correct Photo's UploadDate Inserted", d)
+					return true
+				} else {
+					fmt.Println("InCorrect Photo's UploadDate Inserted", d)
+					return false
+				}
+
+			} else {
+				fmt.Println("InCorrect  Photo's UploadDate Inserted", d)
+				return false
+			}
+		} else {
+			fmt.Println("InCorrect  Photo's UploadDate Inserted", d)
+			return false
+		}
+	} else {
+		fmt.Println("Error encountered!")
+		return false
+	}
 }
 
 type Location struct {
@@ -28,42 +74,90 @@ func (p Phrase) ValidPhrase() bool {
 	return len(p) >= 5 && len(p) <= 1000
 }
 
-func (p Photo) ValidPhoto() bool {
-	return p.Location.ValidLocation() && p.Phrase.ValidPhrase() && p.Photoid >= 0 &&
-		p.NumberLikes >= 0 && p.NumberComments >= 0
+// ----- FINAL PHOTO FUNCTION -----
+
+// Function Method used to check for the User Validity.
+func ValidPhoto(photo Photo) bool {
+	return photo.Photoid >= 0 &&
+		photo.UploadDate.ValidUploadDate() &&
+		photo.Location.ValidLocation() &&
+		photo.Phrase.ValidPhrase() &&
+		photo.NumberLikes >= 0 &&
+		photo.NumberComments >= 0
 }
+
+// -----                -----
 
 type Comment struct {
-	Commentid         int    `json:"commentid"`
-	CommenterUsername string `json:"commenterUsername"`
-	Phrase            Phrase `json:"phrase"`
-	UploadDate        string `json:"uploadDate"`
+	Commentid         int           `json:"commentid"`
+	CommenterUsername FixedUsername `json:"commenterUsername"`
+	Phrase            Phrase        `json:"phrase"`
+	UploadDate        DateTime      `json:"uploadDate"`
 }
 
-func (c Comment) ValidComment() bool {
-	return c.Commentid >= 0 && c.Phrase.ValidPhrase()
+// ----- FINAL PHOTO FUNCTION -----
+
+// Function Method used to check for the User Validity.
+func ValidComment(comment Comment) bool {
+	return comment.Commentid >= 0 &&
+		comment.CommenterUsername.ValidFixedUsername(regex_fixed_username) &&
+		comment.Phrase.ValidPhrase() &&
+		comment.UploadDate.ValidUploadDate()
 }
+
+// -----                -----
 
 type Like struct {
-	Likeid     string `json:"likeid"` //This corresponds to the Username of the Liker.
-	UploadDate string `json:"uploadDate"`
+	Likeid     FixedUsername `json:"likeid"` //This corresponds to the Username of the Liker.
+	UploadDate DateTime      `json:"uploadDate"`
 }
+
+// ----- FINAL LIKE FUNCTION -----
+
+// Function Method used to check for the User Validity.
+func ValidLike(like Like) bool {
+	return like.Likeid.ValidFixedUsername(regex_fixed_username) &&
+		like.UploadDate.ValidUploadDate()
+}
+
+// -----                -----
 
 type Ban struct {
-	Banid      string `json:"banid"` //This corresponds to the Username of the User Banned.
-	UploadDate string `json:"uploadDate"`
-	Motivation string `json:"motivation"`
+	Banid      FixedUsername `json:"banid"` //This corresponds to the Username of the User Banned.
+	UploadDate DateTime      `json:"uploadDate"`
+	Motivation Motivation    `json:"motivation"`
 }
 
-func (b Ban) ValidBan() bool {
-	b_lower := strings.ToLower(b.Motivation)
-	return b_lower == "spam" || b_lower == "bad behaviour" || b_lower == "threats"
+func (m Motivation) ValidMotivation() bool {
+	m_lower := strings.ToLower(string(m))
+	return m_lower == "spam" || m_lower == "bad behaviour" || m_lower == "threats"
 }
+
+// ----- FINAL BAN FUNCTION -----
+
+// Function Method used to check for the User Validity.
+func ValidBan(ban Ban) bool {
+	return ban.Banid.ValidFixedUsername(regex_fixed_username) &&
+		ban.UploadDate.ValidUploadDate() &&
+		ban.Motivation.ValidMotivation()
+}
+
+// -----                -----
 
 type Follow struct {
-	Followid   string `json:"followid"` //This corresponds to the Username of the Follower.
-	UploadDate string `json:"uploadDate"`
+	Followid   FixedUsername `json:"followid"` //This corresponds to the Username of the Follower.
+	UploadDate DateTime      `json:"uploadDate"`
 }
+
+// ----- FINAL FOLLOW FUNCTION -----
+
+// Function Method used to check for the User Validity.
+func ValidFollow(follow Follow) bool {
+	return follow.Followid.ValidFixedUsername(regex_fixed_username) &&
+		follow.UploadDate.ValidUploadDate()
+}
+
+// -----                -----
 
 // Create a JSON Error Message Structure.
 type JSONErrorMsg struct {
--- service/api/file-struct-user.go
+++ service/api/file-struct-user.go
@@ -24,28 +24,30 @@ type Surname string
 type Date string
 type Email string
 type Nationality string
+type Gender string
 
 // Variables Declaration.
 var regex_fixed_username string = "[a-zA-Z0-9]+$"
 var regex_uuid string = "^[0-9a-fA-F-]{36}"         //123e4567-e89b-12d3-a456-426614174000
 var regex_date = "^[0-9]{1,2}/[0-9]{1,2}/[0-9]{4}$" //Without February Check.
 
-var current_year, current_month, current_day = time.Now().Date()
+var current_year, c_month, current_day = time.Now().Date()
+var current_month = int(c_month)
 
 // Create a User structure.
 // (Option + 9 on mac for putting the ` character). They are used for allowing to put the name as JSON RFC Standard Specifications.
 type User struct {
-	Uuid            Uuid          `json:"uuid"`
-	FixedUsername   FixedUsername `json:"fixedUsername"`
-	Username        Username      `json:"username"`
-	PhotoProfile    byte          `json:"photoProfile"`
-	PersonalInfo    PersonalInfo  `json:"personalInfo"`
-	DateOfCreation  string        `json:"dateOfCreation"`
-	NumberOfPhotos  int           `json:"numberOfPhotos"`
-	TotNumberLikes  int           `json:"totNumberLikes"`
-	NumberComments  int           `json:"numberComments"`
-	NumberFollowers int           `json:"numberFollowers"`
-	NumberFollowing int           `json:"numberFollowing"`
+	Uuid              Uuid          `json:"uuid"`
+	FixedUsername     FixedUsername `json:"fixedUsername"`
+	Username          Username      `json:"username"`
+	PhotoProfile      byte          `json:"photoProfile"`
+	PersonalInfo      PersonalInfo  `json:"personalInfo"`
+	DateOfCreation    Date          `json:"dateOfCreation"`
+	NumberOfPhotos    int           `json:"numberOfPhotos"`
+	TotNumberLikes    int           `json:"totNumberLikes"`
+	TotNumberComments int           `json:"totNumberComments"`
+	NumberFollowers   int           `json:"numberFollowers"`
+	NumberFollowing   int           `json:"numberFollowing"`
 }
 
 // Ceation of a sub-Structure that handles the Personal Information of the User.
@@ -55,7 +57,7 @@ type PersonalInfo struct {
 	DateOfBirth Date        `json:"dateOfBirth"`
 	Email       Email       `json:"email"`
 	Nationality Nationality `json:"nationality"`
-	Gender      string      `json:"gender"`
+	Gender      Gender      `json:"gender"`
 }
 
 // Declaring a Method for checking the uuid validty w.r.t. its Regex.
@@ -117,16 +119,79 @@ func (d Date) ValidDateofBirth() bool { //yyyy/mm/dd
 
 	if erry != nil && errm != nil && errd != nil {
 		//Here assume that the user should have been born more than 10 year ago.
-		if year >= 1900 && year <= current_year-10 && month >= 1 && month <= 12 && day >= 1 {
+		if len(d) == 10 && year >= 1900 && year <= current_year-10 && month >= 1 && month <= 12 && day >= 1 {
 			if month == 2 && (day != 29 && day != 30 && day != 31) {
-				fmt.Println("Correct Date of Birth Inserted", d)
-				return true
+				if (month == 4 || month == 6 || month == 9 || month == 11) && (day != 31) {
+					fmt.Println("Correct Date of Birth Inserted", d)
+					return true
+				} else {
+					fmt.Println("InCorrect Date of Birth Inserted", d)
+					return false
+				}
 			} else {
 				fmt.Println("InCorrect Date of Birth Inserted", d)
 				return false
 			}
 		} else {
 			fmt.Println("InCorrect Date of Birth Inserted", d)
+			return false
+		}
+	} else {
+		fmt.Println("Error encountered!")
+		return false
+	}
+}
+
+// Declaring a Method for checking the Email validity w.r.t. its length.
+func (e Email) ValidEmail() bool {
+	email_at := strings.Split(string(e), "-")
+	pre_at := email_at[0]
+	post_at := email_at[1]
+	if len(pre_at) >= 1 && len(post_at) >= 1 && len(e) <= 100 {
+		fmt.Println("Valid Email Inserted!")
+		return true
+	} else {
+		fmt.Println("Not Valid Email Inserted!")
+		return false
+	}
+}
+
+// Declaring a Method for checking the Nationality validity w.r.t. its length.
+func (n Nationality) ValidNationality() bool {
+	return len(n) >= 3 && len(n) <= 100
+}
+
+// Declaring a Method for checking the Gender validity w.r.t. it belongs to an "enum" of values.
+func (g Gender) ValidGender() bool {
+	g_lower := strings.ToLower(string(g))
+	return g_lower == "male" || g_lower == "female" || g_lower == "do not specify"
+}
+
+// Declaring a Method for checking the DateOfCreation validity w.r.t. its validity.
+func (d Date) ValidDateofCreation() bool { //yyyy/mm/dd
+	date := strings.Split(string(d), "-") //here find the way to also include "/"
+	year, erry := strconv.Atoi(date[0])
+	month, errm := strconv.Atoi(date[1])
+	day, errd := strconv.Atoi(date[2])
+
+	if erry != nil && errm != nil && errd != nil {
+		// Check whether the date is after the 2022-01-01 and before the current date (range included).
+		if len(d) == 10 && year <= 2022 && year <= current_year && month >= 1 && month <= 12 && month <= current_month && day >= 1 && current_day <= 31 && day <= current_day {
+			if month == 2 && (day != 29 && day != 30 && day != 31) {
+				if (month == 4 || month == 6 || month == 9 || month == 11) && (day != 31) {
+					fmt.Println("Correct Date of Creation Inserted", d)
+					return true
+				} else {
+					fmt.Println("InCorrect Date of Creation Inserted", d)
+					return false
+				}
+
+			} else {
+				fmt.Println("InCorrect Date of Creation Inserted", d)
+				return false
+			}
+		} else {
+			fmt.Println("InCorrect Date of Creation Inserted", d)
 			return false
 		}
 	} else {
@@ -144,37 +209,16 @@ func ValidUser(user User, regex string) bool {
 		user.Username.ValidUsername() &&
 		user.PersonalInfo.Name.ValidName() &&
 		user.PersonalInfo.Surname.ValidSurname() &&
-		user.PersonalInfo.DateOfBirth.ValidDateofBirth()
+		user.PersonalInfo.DateOfBirth.ValidDateofBirth() &&
+		user.PersonalInfo.Email.ValidEmail() &&
+		user.PersonalInfo.Nationality.ValidNationality() &&
+		user.PersonalInfo.Gender.ValidGender() &&
+		user.DateOfCreation.ValidDateofCreation() &&
+		user.NumberOfPhotos >= 0 &&
+		user.TotNumberLikes >= 0 &&
+		user.TotNumberComments >= 0 &&
+		user.NumberFollowers >= 0 &&
+		user.NumberFollowing >= 0
 }
 
 // -----                -----
-
-// Declaring a Method for checking the Email validity w.r.t. its length.
-func (e Email) ValidEmail() bool {
-	email_at := strings.SplitAfter(d, "-")
-	pre_at, err_pre := strconv.ParseInt(date[0], 10, 32)
-	post_at, err_post := strconv.ParseInt(date[1], 10, 32)
-	if err_pre != nil && err_post != nil {
-		return 1 <= len(e) && 1 <= len(pre_at) && 1 <= len(post_at)
-	} else if err_pre == nil && err_post != nil {
-		fmt.Println("Empty part before the 'at' Character! Wrong Email inserted!")
-		return
-	} else if err_pre != nil && err_post == nil {
-		fmt.Println("Empty part after the 'at' Character! Wrong Email inserted!")
-		return
-	} else if err_pre == nil && err_post != nil {
-		fmt.Println("Empty Email! Wrong Email inserted!")
-		return
-	}
-}
-
-// Declaring a Method for checking the Nationality validity w.r.t. its length.
-func (n Nationality) ValidNationality() bool {
-	return 1 <= len(n)
-}
-
-// Declaring a Method for checking the Gender validity w.r.t. it belongs to an "enum" of values.
-func (f User) ValidGender() bool {
-	g_lower := strings.ToLower(f.PersonalInfo.Gender)
-	return g_lower == "male" || g_lower == "female" || g_lower == "do not specify"
-}
