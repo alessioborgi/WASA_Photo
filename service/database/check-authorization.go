bd5dcc0738a2511d984b7dc84cfe95cd2e97883b
Alessio_Borgi <73711270+alessioborgi@users.noreply.github.com>
2022-12-04 09:27:38 +0100

    Add CheckAuthorization

---
+++ service/database/check-authorization.go
@@ -0,0 +1,26 @@
+package database
+
+func (db *appdbimpl) CheckAuthorization(fixedUsername string, uuid string) (string, error) {
+
+	//Check whether the uuid that is requesting the action is the owner of the profile.
+	var auth bool
+	err := db.c.QueryRow(`SELECT uuid == '?' 
+	FROM Users 
+	WHERE fixedUsername == '?'`, uuid, fixedUsername).Scan(&auth)
+
+	//Check for the error during the Query.
+	if err != nil {
+		return "", err
+	} else {
+		//Returning "Authorized" if it is the owner, "Not Authorized" otherwise.
+		if auth == true {
+			return "Authorized", nil
+		} else {
+			return "Not Authorized", nil
+		}
+	}
+}
+
+// (`SELECT iif(uuid == '?', "Authorized", "Not Authorized" )
+// 	FROM Users
+// 	WHERE fixedUsername == '?'`, uuid, fixedUsername)
--- service/database/get-user-profile.go
+++ service/database/get-user-profile.go
@@ -7,58 +7,38 @@ func (db *appdbimpl) GetUserProfile(fixedUsername string, uuid string) (User, er
 	// Selection of the User profile. Here we can distinguish two cases:
 	//1) We have that the User Profile must return sort of restricted data(i.e., without Personal Data if the User is requesting the action is not the owner.
 	//2) We have that the User Profile must return all data of the profile if the User that is requesting the action is the owner of the profile.
-	authorization, err1 := db.c.Query(`SELECT iif(uuid == '?', CAST("Authorized" AS TEXT), CAST("Not Authorized" AS TEXT))
-	FROM Users
-	WHERE fixedUsername == '?'`, uuid, fixedUsername)
-	if err1 != nil {
-		return user, err1
-	}
+	authorization, errAuth := db.CheckAuthorization(fixedUsername, uuid)
 
-	// Go checking whether you are authorized or not.
-	for authorization.Next() {
-		var auth string
-		err1 = authorization.Scan(&auth)
-		if err1 != nil {
-			return user, err1
-		} else {
+	//Check for the error during the Query.
+	if errAuth != nil {
+		return User{}, errAuth
+	} else {
+		// Go checking whether you are authorized or not(i.e., whether you are the owner of the profile or not).
+		if authorization == "Authorized" {
+			//If you are the Owner of the Profile.
+			err := db.c.QueryRow(`SELECT * 
+			FROM Users
+			WHERE fixedUsername == '?'`, fixedUsername).Scan(&user.FixedUsername, &user.Uuid, &user.Username, &user.PhotoProfile, &user.Biography, &user.DateOfCreation, &user.NumberOfPhotos, &user.TotNumberLikes, &user.TotNumberComments, &user.NumberFollowers, &user.NumberFollowing, &user.Name, &user.Surname, &user.DateOfBirth, &user.Email, &user.Nationality, &user.Gender)
 
-			//If no errors occurs and you are Authorized, simply select all the User Row since the one that is requesting the row is the owner of the profile.
-			if auth == "Authorized" {
-				rows, err := db.c.Query(`SELECT * 
-				FROM Users
-				WHERE fixedUsername == '?'`, fixedUsername)
-
-				// Here we read the resultset and we build the list to be returned.
-				for rows.Next() {
-					err = rows.Scan(&user.FixedUsername, &user.Uuid, &user.Username, &user.PhotoProfile, &user.Biography, &user.DateOfCreation, &user.NumberOfPhotos, &user.TotNumberLikes, &user.TotNumberComments, &user.NumberFollowers, &user.NumberFollowing, &user.Name, &user.Surname, &user.DateOfBirth, &user.Email, &user.Nationality, &user.Gender)
-					if err != nil {
-						return user, err
-					}
-				}
-				if rows.Err() != nil {
-					return user, err
-				}
-				return user, nil
+			//Check for the error during the Query.
+			if err != nil {
+				return User{}, err
 			} else {
-				//In the case no errors occurs but we have an "unauthorized" access, means that the user that is requesting the action is not the owner, thus must have a restricted View of the User profile (i.e, it must not see the Personal Data).
-				//Notice that here, I could have also used a View instead of a query with all these selected columns. I have just opted for the worst choice, since it is very verbose.
-				rows, err := db.c.Query(`SELECT fixedusername, username, photoprofile, biography, dateOfCreation, numberOfPhotos, totNumberLikes, totNumberComments, numberFollowers, numberFollowing
-				FROM Users
-				WHERE fixedUsername == '?'`, fixedUsername)
+				return user, nil
+			}
+		} else {
+			//In the case you are not the profile owner, i.e. you result as "unauthorized", you must have a restricted View of the User profile (i.e, it must not see the Personal Data).
+			//Notice that here, I could have also used a View instead of a query with all these selected columns. I have just opted for the worst choice, since it is very verbose.
+			err := db.c.QueryRow(`SELECT fixedusername, username, photoprofile, biography, dateOfCreation, numberOfPhotos, totNumberLikes, totNumberComments, numberFollowers, numberFollowing
+			FROM Users
+			WHERE fixedUsername == '?'`, fixedUsername).Scan(&user.FixedUsername, &user.Uuid, &user.Username, &user.PhotoProfile, &user.Biography, &user.DateOfCreation, &user.NumberOfPhotos, &user.TotNumberLikes, &user.TotNumberComments, &user.NumberFollowers, &user.NumberFollowing)
 
-				// Here we read the resultset and we build the list to be returned.
-				for rows.Next() {
-					err = rows.Scan(&user.FixedUsername, &user.Uuid, &user.Username, &user.PhotoProfile, &user.Biography, &user.DateOfCreation, &user.NumberOfPhotos, &user.TotNumberLikes, &user.TotNumberComments, &user.NumberFollowers, &user.NumberFollowing)
-					if err != nil {
-						return user, err
-					}
-				}
-				if rows.Err() != nil {
-					return user, err
-				}
+			//Check for the error during the Query.
+			if err != nil {
+				return User{}, err
+			} else {
 				return user, nil
 			}
 		}
 	}
-	return user, nil
 }
