5eb814a55332e06077b235daf1cde1b0de4b681d
Alessio_Borgi <73711270+alessioborgi@users.noreply.github.com>
2023-01-01 23:36:50 +0100

    Css Login

--- webui/src/App.vue
+++ webui/src/App.vue
@@ -8,7 +8,7 @@ export default {}
 <template>
 
 	<header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-		<a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#/">WASAPhoto</a>
+		<a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#/">WASA Photo</a>
 		<button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
@@ -24,32 +24,44 @@ export default {}
 					<ul class="nav flex-column">
 						<li class="nav-item">
 							<RouterLink to="/doLogin" class="nav-link">
-								<svg class="feather"><use href="/feather-sprite-v4.29.0.svg#home"/></svg>
+								<svg class="feather"><use href="/feather-sprite-v4.29.0.svg#log-in"/></svg>
 								Login
 							</RouterLink>
 						</li>
 						<li class="nav-item">
 							<RouterLink to="/link1" class="nav-link">
-								<svg class="feather"><use href="/feather-sprite-v4.29.0.svg#layout"/></svg>
-								Menu item 1
+								<svg class="feather"><use href="/feather-sprite-v4.29.0.svg#home"/></svg>
+								Home
 							</RouterLink>
 						</li>
 						<li class="nav-item">
 							<RouterLink to="/link2" class="nav-link">
-								<svg class="feather"><use href="/feather-sprite-v4.29.0.svg#key"/></svg>
-								Menu item 2
+								<svg class="feather"><use href="/feather-sprite-v4.29.0.svg#instagram"/></svg>
+								My Profile
+							</RouterLink>
+						</li>
+						<li class="nav-item">
+							<RouterLink to="/link2" class="nav-link">
+								<svg class="feather"><use href="/feather-sprite-v4.29.0.svg#image"/></svg>
+								New Photo
 							</RouterLink>
 						</li>
 					</ul>
 
 					<h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
-						<span>Secondary menu</span>
+						<span>Profile</span>
 					</h6>
 					<ul class="nav flex-column">
 						<li class="nav-item">
 							<RouterLink :to="'/some/' + 'variable_here' + '/path'" class="nav-link">
-								<svg class="feather"><use href="/feather-sprite-v4.29.0.svg#file-text"/></svg>
-								Item 1
+								<svg class="feather"><use href="/feather-sprite-v4.29.0.svg#lock"/></svg>
+								Ban
+							</RouterLink>
+						</li>
+						<li class="nav-item">
+							<RouterLink :to="'/some/' + 'variable_here' + '/path'" class="nav-link">
+								<svg class="feather"><use href="/feather-sprite-v4.29.0.svg#user-check"/></svg>
+								Follow
 							</RouterLink>
 						</li>
 					</ul>
--- webui/src/views/DoLoginView.vue
+++ webui/src/views/DoLoginView.vue
@@ -1,3 +1,7 @@
+<style>
+  @import '/Users/alessioborgi/Documents/GitHub/WASA_Photo/webui/src/assets/login.css';
+</style>
+
 <script>
 export default {
     data: function() {
@@ -35,69 +39,17 @@ export default {
 </script>
 
 <template>
-    <!-- <div>
-        <div
-            class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-            <h1 class="h2">Login</h1>
-        </div>
+    
+    <div class="login">
+        <h1>Login</h1>
         <ErrorMsg v-if="errormsg" :msg="errormsg"></ErrorMsg>
-
-        <div class="mb-3">
-            <label for="description" class="form-label">Username</label>
-            <input type="string" class="form-control" id="username" v-model="Username" placeholder="please insert username">
-        </div>
-
-        <div>
-            <button v-if="!loading" type="button" class="btn btn-primary" @click="LoginUser">
-                Login
-            </button>
+        <form method="post">
+            <input type="string" id="username" name="u" v-model="Username" placeholder="Username..." required="required" />
+            <button type="submit" class="btn btn-primary btn-block btn-large" @click="LoginUser">Login</button>
             <LoadingSpinner v-if="loading"></LoadingSpinner>
-        </div>
-    </div> -->
-
-    <div class='login'>
-        <div class='login_title'>
-            <span>LOGIN</span>
-        </div>
-        <div class='login_fields'>
-            <div class='login_fields__user'>
-                <div class='icon'>
-                    <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/user_icon_copy.png'>
-                </div>
-                <input placeholder='Username' type='text'>
-                    <div class='validation'>
-                        <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/tick.png'>
-                    </div>
-            </div>
-            <div class='login_fields__submit'>
-                <input type='submit' value='Log In'>
-                <div class='forgot'>
-                    <a href='#'></a>
-                </div>
-            </div>
-        </div>
-        <div class='success'>
-            <h2>Authentication Success</h2>
-            <p>Welcome back</p>
-        </div>
-        <div class='disclaimer'>
-            <p>@AlessioBorgi WASAPhoto Project</p>
-        </div>
+        </form>
     </div>
-    <div class='authent'>
-        <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/puff.svg'>
-        <p>Authenticating...</p>
-    </div>
-
-
 </template>
-
-<style>
-</style>
-
-
-
-
 
 
 <!-- For doing the heart!!! -->
