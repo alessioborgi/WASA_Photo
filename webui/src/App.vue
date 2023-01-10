b9297e08ab3d153cdaa47113654eb2c6afcac49e
Alessio_Borgi <73711270+alessioborgi@users.noreply.github.com>
2023-01-10 23:00:59 +0100

    App Changed

--- webui/src/App.vue
+++ webui/src/App.vue
@@ -8,10 +8,10 @@ export default {}
 <template>
 
 	<header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-		<a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#/">WASA Photo</a>
-		<button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+		<a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-7" href="/">WASA Photo</a>
+		<!-- <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
-		</button>
+		</button> -->
 	</header>
 
 	<div class="container-fluid">
