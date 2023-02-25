22ea74a708e200ec91d2305eb65ffd7fe4aa9025
Alessio_Borgi <73711270+alessioborgi@users.noreply.github.com>
2023-02-25 16:01:46 +0100

    Fix NewPhoto

--- webui/src/views/NewPhotoView.vue
+++ webui/src/views/NewPhotoView.vue
@@ -116,6 +116,16 @@ export default {
             this.$refs.fileInput.click()
         },
 
+        clearPhoto () {
+
+            // Initializing the Background Text on the Image Box.
+            this.photoBackgroundText= "CLICK HERE to CHOOSE A FILE";
+            this.previewImage = null;
+            // Re-addressing the page to the personal profile page of a user.
+            this.$router.push({ path: `/users/${this.username}/photo/` })
+
+        },
+
     },
 }
 </script>
@@ -174,14 +184,21 @@ export default {
                         </div>
                     </div>
 
-                    <!-- Send Button -->
-                    <div class="form-group2">
-					    <button type="login-button" class="btn btn-primary btn-block btn-large" v-if="!loading" @click="uploadPhoto"> Upload New Photo </button>
-				    </div>
-
                 </fieldset>
             </form>
         </div>
+
+        <!-- Clear Button -->
+        <div class="clearButton">
+            <!-- <button class="btn btn-primary btn-block btn-large" @click="clearPhoto">Clear</button> -->
+            <button @click="clearPhoto" style="background: #c2e9fc; border: 1px solid #FF4742; border-radius: 6px; box-shadow: rgba(0, 0, 0, 0.1) 1px 2px 4px; box-sizing: border-box; color: #c2e9fc; cursor: pointer; display: inline-block; font-family: sans-serif; font-size: 16px; font-weight: 800; line-height: 16px; min-height: 40px; outline: 0; padding: 12px 14px; text-align: center; text-rendering: geometricprecision; text-transform: none; user-select: none; -webkit-user-select: none; touch-action: manipulation; vertical-align: middle; background-color: #c2e9fc; background-position: 0 0; color: black; ">
+                Clear</button>
+        </div>
+
+        <!-- Upload Photo Button -->
+        <div class="form-group2">
+		    <button type="login-button" class="btn btn-primary btn-block btn-large" v-if="!loading" @click="uploadPhoto"> Upload New Photo </button>
+		</div>
     </div><!-- /.container -->
 </template>
 
